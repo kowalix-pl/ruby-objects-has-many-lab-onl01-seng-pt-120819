c204901e0143839cc96877e7041aaaaca4521266
kowalix007 <kowalix007@gmail.com>
2020-01-27 16:48:23 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Song 
+  
+attr_accessor :name, :artist
+ @@all = []
+ def initialize (name)
+  @name = name
+  @@all << self
+ end 
+ 
+ def self.all 
+ @@all
+ end 
+  #binding.pry 
+ 
+ def artist_name
+   self.artist.name
+ end
+ 
+ def song
+   Song.all.select {|song| song.artist == lil_nas_x}
+ end 
+ 
+end
